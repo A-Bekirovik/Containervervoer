5db07738038a43e84a4b7a733a73fb2b3a03e2d7
arvenbekirovik <514816@student.fontys.nl>
2024-06-15 16:57:38 +0200

    Worked out the Ship class
    Also Added OOP

---
+++ ContainerVervoer/Row.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ContainerVervoer
+{
+	// This is the first row in every ship
+
+	public class Row
+	{
+		public Row()
+		{
+		}
+	}
+}
+
---
+++ ContainerVervoer/Stack.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ContainerVervoer
+{
+	public class Stack
+	{
+		public Stack()
+		{
+		}
+	}
+}
+
