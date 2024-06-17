7dccefe37e2da0c1d3d0d198d55dc7405b46a571
arvenbekirovik <514816@student.fontys.nl>
2024-06-17 14:11:27 +0200

    Need a way to fix program starting my program

---
+++ ContainerVervoer/ContainerStarter.cs
@@ -0,0 +1,15 @@
+﻿using ContainerVervoer;
+using System;
+
+namespace ContainerVervoer
+{
+    public class ContainerPlacer
+    {
+        public readonly Ship Ship;
+
+        public ContainerPlacer(Ship ship)
+        {
+            Ship = ship;
+        }
+    }
+}
--- ContainerVervoer/Program.cs
+++ ContainerVervoer/Program.cs
@@ -1,3 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using ContainerVervoer;
 
-Console.WriteLine("IKAKNERNKA");
+namespace ContainerVervoer
+{
+    public partial class ContainerShip
+    {
+
+        List<Container> FormContainers = new List<Container>();
+
+        public ContainerShip()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            ContainerPlacer containerPlacer = new ContainerPlacer(new Ship(6, 3));
+
+            FormContainers.Add(new Container(30, true, false));
+
+            foreach (Container container in FormContainers)
+            {
+                containerPlacer.Ship.Containers.Add(container);
+            }
+
+            containerPlacer.Ship.Run();
+        }
+    }
+}
